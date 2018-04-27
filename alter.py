94d2a8c7211e2f1aa86ce73859780a086179fed2
user\lev <levkrutogolov@gmail.com>
2018-04-27 20:51:46 +0300

    TTS + STT, working not so good

--- alter.py
+++ alter.py
@@ -1,14 +1,45 @@
-# This program consist of three parts:
-# 1. Turning given text to speech
-# 2. Adding good enough noise to the resulting audio file
-# 3. Turning speech back to text, which should be different from the original one.
+# This program consist of two parts:
+# 1. Turning given text to speech (wav file output)
+# 2. Turning speech back to text
 
 import subprocess
+import speech_recognition as sr
+from os import path
+import uuid
 
 espeak_path = "C:/Program Files (x86)/eSpeak/command_line/espeak.exe"
-temp_speech_path = "C:/Users/lev/PycharmProjects/mahanet/2018/"
+temp_speech_path = "D:/Technical/Git/tech-pun-generator/tmp/"
+temp_file_name = str(uuid.uuid4())
 
-def textToWav(text, file_name):
-    subprocess.call([espeak_path, "-w " + temp_speech_path + file_name + ".wav", text])
+starting_text = "[[k'erberos]]"
 
-textToWav("To NTLM or not to NTLM", 'hello')
+def textToWav(text, wav_full_path):
+    subprocess.call([espeak_path, "-w " + wav_full_path, "-g 5", "-ven+whisper", text])
+
+def wavToText(wav_path, output_full_path):
+    r = sr.Recognizer()
+    with sr.AudioFile(wav_path) as source:
+        audio = r.record(source)  # read the entire audio file
+
+    # recognize speech using Sphinx
+    try:
+        output = r.recognize_sphinx(audio)
+        print("Sphinx thinks you said: " + output)
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+
+    if output:
+        try:
+            file = open(output_full_path, "w")
+            file.write(output)
+            file.close()
+        except Exception as exc:
+            print "Exception while writing file: \n{0}".format(exc.message)
+
+temp_full_wav_path = path.join(temp_speech_path, temp_file_name + ".wav")
+textToWav(starting_text, temp_full_wav_path)
+wavToText(temp_full_wav_path, path.join(temp_speech_path, temp_file_name + ".txt"))
+
+
