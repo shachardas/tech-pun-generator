46a974e64629fa7e21239d2a6f4063b6072e63d7
user\lev <levkrutogolov@gmail.com>
2018-04-28 17:10:08 +0300

    Merge branch 'master' of https://github.com/levkr/tech-pun-generator
    # Conflicts:
    #	alter.py
    
    with options

--- config.py
+++
@@ -1,3 +0,0 @@
-espeak_path = "C:/Program Files (x86)/eSpeak/command_line/espeak.exe"
-temp_speech_path = "C:\\Users\\Ariel\\Documents\\temp"
-text_to_speech_path = "C:\\Program Files\\Jampal\\ptts.vbs"
